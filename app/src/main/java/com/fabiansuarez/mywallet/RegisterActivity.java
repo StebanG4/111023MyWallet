28f34398a7c4a3830d11e9188f53156b4c80885e
Juan <jaguirre770@unab.edu.co>
2023-10-13 08:52:02 -0500

    Version Botones Actualizado

--- app/src/main/java/com/fabiansuarez/mywallet/RegisterActivity.java
+++ app/src/main/java/com/fabiansuarez/mywallet/RegisterActivity.java
@@ -2,7 +2,10 @@ package com.fabiansuarez.mywallet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button btnRegistro = findViewById(R.id.volverbtn);
+
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
